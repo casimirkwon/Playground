0e781bdf8ee054c772b65ac8b66a27a845723e6e
hjk7902 <hjk7902@gmail.com>
2017-07-28 15:17:45 +0900

    commit

--- src/main/java/APITest4.java
+++ src/main/java/APITest4.java
@@ -1,15 +1,29 @@
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import org.apache.http.client.fluent.Request;
+
 public class APITest4 {
 	// TODO [실습 3-02] http client를 이용하여 API 호출을 수행한 후, response body(json 데이터)를 출력한다.
 	
 	
-	/* hint
-    	
-    	use httpclient. requires fluent-hc module for fluent style method call.
-    	
-		Content content = Request.Get("https://www.google.com")
-			.execute()
-			.returnContent();
-			
-	
-	 */
+	public static void main(String[] args) throws IOException {
+		
+		StringBuilder urlBuilder = new StringBuilder(
+				"https://sandbox-apigw.koscom.co.kr/v2/market/stocks/{marketcode}/closeindex".replace("{marketcode}",
+						URLEncoder.encode("kospi", "UTF-8")));
+		urlBuilder.append("?");
+		urlBuilder.append(URLEncoder.encode("apikey", "UTF-8") + "="
+				+ URLEncoder.encode("l7xx230ef2235e34448c982eb192ac98e206", "UTF-8"));
+
+		// http response 획득. using fluent style method call.
+		String response = Request.Get(urlBuilder.toString())
+									.execute()
+									.returnContent()
+									.asString();
+		
+		// http response body 출력
+		System.out.println(response);
+	}
+
 }
--- src/main/java/APITest6.java
+++ src/main/java/APITest6.java
@@ -1,15 +1,34 @@
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import org.apache.http.client.fluent.Request;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class APITest6 {
 	// TODO [실습 3-03] jackson 라이브러리를 사용하여 API 호출로 획득한 시세 데이터 json object 내용 중 원하는 값(ex. 현재가)만 출력한다.
 	
-	
-	/* hint
-    	
-    	retrieve "result" object from root json data 
-    	
+	public static void main(String[] args) throws IOException {
+
+		StringBuilder urlBuilder = new StringBuilder(
+				"https://sandbox-apigw.koscom.co.kr/v2/market/stocks/{marketcode}/closeindex".replace("{marketcode}",
+						URLEncoder.encode("kospi", "UTF-8")));
+		urlBuilder.append("?");
+		urlBuilder.append(URLEncoder.encode("apikey", "UTF-8") + "="
+				+ URLEncoder.encode("l7xx230ef2235e34448c982eb192ac98e206", "UTF-8"));
+
+		String data = Request.Get(urlBuilder.toString())
+				.execute()
+				.returnContent()
+				.asString();
+		
 		ObjectMapper objectMapper = new ObjectMapper();
 		ObjectNode root = objectMapper.readValue(data, ObjectNode.class);
-		System.out.println(root.get("result").toString());
-			
-	
-	 */
+		JsonNode trdPrc = root.get("result").get("trdPrc");
+		
+		System.out.println("trdPrc : " + trdPrc.asDouble());
+
+	}	
 }
